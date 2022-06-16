4a658ea91fefc5cb9c1933b73af9e33905ebb51a
Jason <ya850805@gmail.com>
2022-06-16 23:06:24 +0800

    feat: Add dead letter demo publisher.

---
+++ rabbitmq-hello/src/main/java/com/practice/rabbitmq/eighth/Producer.java
@@ -0,0 +1,30 @@
+package com.practice.rabbitmq.eighth;
+
+import com.practice.rabbitmq.utils.RabbitMqUtils;
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * 死信隊列
+ * 生產者
+ */
+public class Producer {
+    //普通交換機名稱
+    public static final String NORMAL_EXCHANGE = "normal_exchange";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        Channel channel = RabbitMqUtils.getChannel();
+
+        //延遲消息，設置TTL的時間，單位是ms
+        AMQP.BasicProperties properties = new AMQP.BasicProperties().builder().expiration("10000").build();
+
+        for (int i = 1; i < 11; i++) {
+            String message = "info" + i;
+            channel.basicPublish(NORMAL_EXCHANGE, "zhangsan", properties, message.getBytes());
+        }
+    }
+}
