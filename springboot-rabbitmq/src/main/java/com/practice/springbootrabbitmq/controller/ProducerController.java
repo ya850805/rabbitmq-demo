bdd0e1d5ef9aac764686ed4001518c492677d301
Jason <ya850805@gmail.com>
2022-07-20 01:41:36 +0800

    feat: Add different situations to send message.

--- springboot-rabbitmq/src/main/java/com/practice/springbootrabbitmq/controller/ProducerController.java
+++ springboot-rabbitmq/src/main/java/com/practice/springbootrabbitmq/controller/ProducerController.java
@@ -2,6 +2,7 @@ package com.practice.springbootrabbitmq.controller;
 
 import com.practice.springbootrabbitmq.config.ConfirmConfig;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +20,24 @@ public class ProducerController {
     @GetMapping("/sendMessage/{message}")
     public void sendMessage(@PathVariable("message") String message) {
         log.info("發送消息內容：{}", message);
-        rabbitTemplate.convertAndSend(ConfirmConfig.CONFIRM_EXCHANGE_NAME, ConfirmConfig.CONFIRM_ROUTING_KEY, message);
+
+        /**
+         * id設定為1
+         */
+        CorrelationData correlationData = new CorrelationData("1");
+        /**
+         * 正確的交換機，發送消息成功
+         */
+        rabbitTemplate.convertAndSend(ConfirmConfig.CONFIRM_EXCHANGE_NAME, ConfirmConfig.CONFIRM_ROUTING_KEY, message, correlationData);
+        /**
+         * 錯誤(未存在)的交換機，發送消息失敗
+         */
+        rabbitTemplate.convertAndSend(ConfirmConfig.CONFIRM_EXCHANGE_NAME + "123", ConfirmConfig.CONFIRM_ROUTING_KEY, message, correlationData);
+
+        CorrelationData correlationData2 = new CorrelationData("2");
+        /**
+         * 錯誤的RoutingKey，發送消息失敗
+         */
+        rabbitTemplate.convertAndSend(ConfirmConfig.CONFIRM_EXCHANGE_NAME, ConfirmConfig.CONFIRM_ROUTING_KEY + "xxx", message, correlationData2);
     }
 }
