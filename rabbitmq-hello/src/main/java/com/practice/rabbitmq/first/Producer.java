7c195299f46e3c7b4e553f6254e68b764d2a0471
Jason <ya850805@gmail.com>
2022-07-25 01:15:16 +0800

    feat: Add priority queue and send priority message.

--- rabbitmq-hello/src/main/java/com/practice/rabbitmq/first/Producer.java
+++ rabbitmq-hello/src/main/java/com/practice/rabbitmq/first/Producer.java
@@ -1,10 +1,13 @@
 package com.practice.rabbitmq.first;
 
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
 /**
@@ -39,10 +42,23 @@ public class Producer {
          * 4. 是否自動刪除。最後一個消費者端開連接以後，該隊列是否自動刪除。true: 自動刪除，false: 不自動刪除
          * 5. 其他參數
          */
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        Map<String, Object> arguments = new HashMap<>();
+        arguments.put("x-max-priority", 10); //官方允許是0-255，此處設置10，允許範圍為0-10，不要設置過大會浪費CPU與內存
+        channel.queueDeclare(QUEUE_NAME, true, false, false, arguments);
+
+        for (int i = 1; i < 11; i++) {
+            String message = "info" + i;
+            if(i == 5) {
+                //第5條消息優先級設置為5
+                AMQP.BasicProperties properties = new AMQP.BasicProperties().builder().priority(5).build();
+                channel.basicPublish("", QUEUE_NAME, properties, message.getBytes());
+            } else {
+                channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            }
+        }
 
         //欲發送的消息
-        String message = "hello world!!!";
+//        String message = "hello world!!!";
 
         /**
          * 發送一個消息
@@ -51,7 +67,7 @@ public class Producer {
          * 3. 其他參數信息
          * 4. 發送消息體
          */
-        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+//        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
 
         System.out.println("消息發送完畢");
     }
